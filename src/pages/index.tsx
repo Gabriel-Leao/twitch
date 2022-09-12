0822d30b9227e475b94c0b1fba8a8ff24d7594de
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-09-12 18:57:16 -0300

    Beginning the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Twitch</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Este site é um clone, não é a twitch de verdade" />
+        <meta name="keywords" content="Twitch, twitch, live, livestream" />
+        <meta name="author" content="Gabriel Leão" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default Home
